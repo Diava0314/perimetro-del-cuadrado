393db399d213ecf3e77916bce1894c33217d5faf
Diava0314 <juan14diavanera@gamil.com>
2023-04-10 13:08:44 -0500

    cuadrado

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+alert("Hoy vamos a calcular el perimetro a un cuadradro");
+
+let perimetro: number = Number(prompt("asigna el perimetro del cuadradro"));
+let resultado: number = perimetro*4;
+
+alert("tu resultado es:"+ resultado );
